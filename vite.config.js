f5cbc901775e9b5832b76c5c3125cb005243edbf
BangKartavya <bangkartavya@gmail.com>
2026-08-17 14:03:26 +0530

    fix : vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig((command) => {
+export default defineConfig(({command}) => {
   const isProduction = command === 'build';
   
   return {
